fd30c3eae890d6a1e3ab71b45384eef7dc637c4f
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-07-12 17:05:33 +0530

    Added HttpHeaders to support headers in the department service

--- src/app/department/add-dep/add-dep.component.ts
+++ src/app/department/add-dep/add-dep.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog'
 import { DepartmentService } from 'src/app/services/department.service';
 import { NgForm } from '@angular/forms';
+import { Department } from 'src/app/models/department-model';
 
 @Component({
   selector: 'app-add-dep',
@@ -35,7 +36,7 @@ export class AddDepComponent implements OnInit {
 
   onSubmit(form:NgForm){
     //console.log(form.value);
-    this.service.addDepartment(form.value).subscribe(res=>
+    this.service.addDepartment(form.value).subscribe((res : Department)=>
       {
         this.resetForm(form);
         alert(res);
--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Department } from 'src/app/models/department-model';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
@@ -15,12 +15,16 @@ export class DepartmentService {
 
   readonly APIUrl = "http://localhost:9999/department/";
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   getDepList() : Observable<Department[]>{
     return this.http.get<Department[]>(this.APIUrl + 'show');
   }
 
   addDepartment(dep:Department){
-    return this.http.post(this.APIUrl + 'add', this.formData);
+    return this.http.post(this.APIUrl + 'add', this.formData, this.httpOptions);
   }
 
   private _listeners = new Subject<any>();
